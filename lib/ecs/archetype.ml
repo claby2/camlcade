ecf37b8a9aff27b961b2dddd3df11aeb7fa47e67
Edward Wibowo <wibow9770@gmail.com>
2024-12-19 23:53:01 -0500

    refactor: maintain invariants in archetype

--- lib/ecs/archetype.ml
+++ lib/ecs/archetype.ml
@@ -10,29 +10,35 @@ module Edges = struct
     type 'a t = { add : 'a option; remove : 'a option }
 
     let empty = { add = None; remove = None }
-    let with_add e add = { e with add }
-    let with_remove e remove = { e with remove }
+    let set_add e add = { e with add }
+    let set_remove e remove = { e with remove }
+    let add e = e.add
+    let remove e = e.remove
   end
 
   type ('a, 'b) t = ('a, 'b Edge.t) Hashtbl.t
 
-  let empty () : ('a, 'b) t = Hashtbl.create 0
+  let empty () = Hashtbl.create 0
 
-  let find_add_opt (e : ('a, 'b) t) key =
-    match Hashtbl.find_opt e key with Some edge -> edge.add | None -> None
+  let find_add_opt e key =
+    match Hashtbl.find_opt e key with
+    | Some edge -> Edge.add edge
+    | None -> None
 
-  let find_remove_opt (e : ('a, 'b) t) key =
-    match Hashtbl.find_opt e key with Some edge -> edge.remove | None -> None
+  let find_remove_opt e key =
+    match Hashtbl.find_opt e key with
+    | Some edge -> Edge.remove edge
+    | None -> None
 
   let replace_add e key value =
     match Hashtbl.find_opt e key with
-    | Some edge -> Hashtbl.replace e key (Edge.with_add edge value)
-    | None -> Hashtbl.add e key (Edge.with_add Edge.empty value)
+    | Some edge -> Hashtbl.replace e key (Edge.set_add edge value)
+    | None -> Hashtbl.add e key (Edge.set_add Edge.empty value)
 
   let replace_remove e key value =
     match Hashtbl.find_opt e key with
-    | Some edge -> Hashtbl.replace e key (Edge.with_remove edge value)
-    | None -> Hashtbl.add e key (Edge.with_remove Edge.empty value)
+    | Some edge -> Hashtbl.replace e key (Edge.set_remove edge value)
+    | None -> Hashtbl.add e key (Edge.set_remove Edge.empty value)
 end
 
 type t = {
@@ -85,18 +91,28 @@ exception Entity_not_found
 (* Remove an entity from the archetype and return its components *)
 let extract_entity a e =
   if not (Id.EntitySet.mem e a.entities) then raise Entity_not_found;
-  let res =
-    Id.ComponentSet.to_list a.components
+
+  (* Gather all components for the entity before mutating the archetype *)
+  let components = Id.ComponentSet.to_list a.components in
+  let extracted =
+    components
     |> List.map (fun cid ->
            let table = Hashtbl.find a.table cid in
-           match Hashtbl.find_opt table e with
-           | Some c ->
-               Hashtbl.remove table e;
-               c
-           | None -> failwith "Entity not found, is this a bug?")
+           let component = Hashtbl.find table e in
+           (cid, component))
   in
+
+  (* Now we know all components exist, we can safely remove the entity *)
+  extracted
+  |> List.iter (fun (cid, _) ->
+         let table = Hashtbl.find a.table cid in
+         Hashtbl.remove table e);
+
+  (* Finally, remove the entity from the entity set *)
   a.entities <- Id.EntitySet.remove e a.entities;
-  res
+
+  (* Return the packed components *)
+  extracted |> List.map snd
 
 exception Invalid_components
 
@@ -105,20 +121,30 @@ let add_entity a e components =
   (* Convert components to a hash table from component id to component value *)
   let components =
     Hashtbl.of_seq
-      (components |> List.to_seq |> Seq.map (fun c -> (Component.id c, c)))
+      (List.to_seq components |> Seq.map (fun c -> (Component.id c, c)))
   in
-  if Hashtbl.length components <> Id.ComponentSet.cardinal a.components then
-    raise Invalid_components;
-  components |> Hashtbl.to_seq
-  |> Seq.iter (fun (id, c) ->
-         if not (Id.ComponentSet.mem id a.components) then
-           raise Invalid_components;
-         match Hashtbl.find_opt a.table id with
-         | Some table -> Hashtbl.add table e c
-         | None ->
-             let table = Hashtbl.create 0 in
-             Hashtbl.add table e c;
-             Hashtbl.add a.table id table);
+
+  (* Check that the given components match the archetype's components *)
+  let expected_count = Id.ComponentSet.cardinal a.components in
+  let actual_count = Hashtbl.length components in
+  if expected_count <> actual_count then raise Invalid_components;
+  Id.ComponentSet.iter
+    (fun cid ->
+      if not (Hashtbl.mem components cid) then raise Invalid_components)
+    a.components;
+
+  (* Add the components to the archetype *)
+  Hashtbl.iter
+    (fun cid c ->
+      match Hashtbl.find_opt a.table cid with
+      | Some table -> Hashtbl.add table e c
+      | None ->
+          let table = Hashtbl.create 0 in
+          Hashtbl.add table e c;
+          Hashtbl.add a.table cid table)
+    components;
+
+  (* Add the entity to the entity set *)
   a.entities <- Id.EntitySet.add e a.entities
 
 let get_component a c e =
--- test/test_ecs_archetype.ml
+++ test/test_ecs_archetype.ml
@@ -38,13 +38,24 @@ let test_edges () =
   assert (Archetype.Edges.find_remove_opt edges 0 = None)
 
 let test_add_entity () =
+  (* Create an archetype with Foo and Bar components *)
   let archetype =
     Archetype.create (Id.ComponentSet.of_list [ Bar.C.id; Foo.C.id ])
   in
+
+  (* Add two entities to the archetype *)
   Archetype.add_entity archetype 0
     [ Component.pack (module Foo.C) 1; Component.pack (module Bar.C) 2 ];
   Archetype.add_entity archetype 1
     [ Component.pack (module Foo.C) 3; Component.pack (module Bar.C) 4 ];
+  assert (Id.EntitySet.cardinal (Archetype.entities archetype) = 2);
+
+  (* Add the same entity again, the number of entities should not change *)
+  Archetype.add_entity archetype 1
+    [ Component.pack (module Foo.C) 3; Component.pack (module Bar.C) 4 ];
+  assert (Id.EntitySet.cardinal (Archetype.entities archetype) = 2);
+
+  (* Adding entities with different sets of components should raise exceptions *)
   assert (
     try
       Archetype.add_entity archetype 3 [];
@@ -60,9 +71,27 @@ let test_add_entity () =
       Archetype.add_entity archetype 3
         [ Component.pack (module Bar.C) 6; Component.pack (module Bar.C) 7 ];
       false
-    with Archetype.Invalid_components -> true)
+    with Archetype.Invalid_components -> true);
+
+  (* Add an entity with duplicate components, should still work *)
+  Archetype.add_entity archetype 3
+    [
+      Component.pack (module Foo.C) 1;
+      Component.pack (module Bar.C) 1;
+      (* The next two components should overwrite the previous ones *)
+      Component.pack (module Bar.C) 2;
+      Component.pack (module Foo.C) 2;
+    ];
+  assert (Id.EntitySet.cardinal (Archetype.entities archetype) = 3);
+  assert (
+    Archetype.get_component archetype Foo.C.id 3
+    |> Option.get |> Component.unpack |> Foo.C.of_base = 2);
+  assert (
+    Archetype.get_component archetype Bar.C.id 3
+    |> Option.get |> Component.unpack |> Bar.C.of_base = 2)
 
 let test_extract_entity () =
+  (* Create an archetype with Foo and Bar components *)
   let archetype =
     Archetype.create (Id.ComponentSet.of_list [ Bar.C.id; Foo.C.id ])
   in
@@ -72,13 +101,18 @@ let test_extract_entity () =
       false
     with Archetype.Entity_not_found -> true);
 
-  (* Add entity without calling add_entity *)
+  (* Add an entity to the archetype *)
   Archetype.add_entity archetype 0
     [ Component.pack (module Foo.C) 1; Component.pack (module Bar.C) 2 ];
 
-  assert (
-    Archetype.extract_entity archetype 0
-    |> List.map Component.id = [ Foo.C.id; Bar.C.id ])
+  (* Extract the entity and check that it has the correct components *)
+  let extracted = Archetype.extract_entity archetype 0 in
+  assert (extracted |> List.map Component.id = [ Foo.C.id; Bar.C.id ]);
+  assert (Id.EntitySet.cardinal (Archetype.entities archetype) = 0);
+
+  (* Add back the entity and check that the entity set has one entity *)
+  Archetype.add_entity archetype 0 extracted;
+  assert (Id.EntitySet.cardinal (Archetype.entities archetype) = 1)
 
 let () =
   test_hash ();
